49a660c742c798f350e65d9bb608f204131dfbe0
xelk <haveiteasy@gmail.com>
2021-10-28 15:20:55 +0200

    set correct permissions on shared files

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run -it -d --rm -v $(pwd)/share:/root -v ~/.ssh:/tmp/.ssh:ro kube:latest
+docker run -it -d --rm --user "$(id -u):$(id -g)" -v $(pwd)/share:/root -v ~/.ssh:/tmp/.ssh:ro kube:latest
 docker attach $(docker ps -l|tail -n1 |awk '{print $1}')
